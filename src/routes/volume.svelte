780fe4015a5338be582e0c96a49d610acfa977cf
Seth <seth@elklabs.org>
2024-03-07 22:08:06 -0500

    chore: update volume data w/ liquidated collateral

--- src/routes/volume.svelte
+++ src/routes/volume.svelte
@@ -129,76 +129,212 @@ onMount(async () => {
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200">
-            <thead class="bg-gray-50">
-              <tr>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  #
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Name
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Total Borrowed
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Total Repaid
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Total Supplied
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Total Withdrawn
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {#if volumeData}
-              {#each volumeData.assets as asset, index}
-              <tr class={(index + 1)%2 == 0 ? "bg-gray-50" : "bg-white"}>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {index + 1}
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                  <div class="text-lg">{asset.name}</div>
-                  <div class="text-sm">{asset.symbol}</div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {#if asset.totalBorrow.totalToken > 0}
-                  <div class="text-lg">{asset.totalBorrow.totalToken.toLocaleString('en-US')}</div>
-                  <div class="text-sm">{USDollar.format(asset.totalBorrow.totalUSD)}</div>
+          <thead class="bg-gray-50">
+            <tr>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                #
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Name
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Total Borrowed
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Total Repaid
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Total Supplied
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Total Withdrawn
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Collateral Liquidated
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {#if volumeData}
+            {#each volumeData as asset, index}
+            <tr class={(index + 1)%2 == 0 ? "bg-gray-50" : "bg-white"}>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {index + 1}
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                <div class="text-lg">{asset.name}</div>
+                <div class="text-sm">{asset.symbol}</div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {#if asset.totalBorrow.totalToken > 0}
+                <div class="text-lg">{asset.totalBorrow.totalToken.toLocaleString('en-US')}</div>
+                <div class="text-sm flex">
+                  <span>{USDollar.format(asset.totalBorrow.totalUSD)}</span>
+                  {#if asset.borrowChange24h == undefined || asset.borrowChange24h > 0}
+                    <p class="flex items-baseline text-sm font-semibold text-green-600">
+                      <!-- Heroicon name: solid/arrow-sm-up -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Increased by
+                      </span>
+                      {`${(asset.borrowChange24h).toFixed(2)}%`}
+                    </p>
                   {:else}
-                  <div class="text-lg"> - </div>
+                    <p class="flex items-baseline text-sm font-semibold text-red-600">
+                      <!-- Heroicon name: solid/arrow-sm-down -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Decreased by
+                      </span>
+                      {`${(asset.borrowChange24h).toFixed(2)}%`}
+                    </p>
                   {/if}
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-lg text-gray-500">
-                  {#if asset.totalRepay.totalToken > 0}
-                  <div class="text-lg">{asset.totalRepay.totalToken.toLocaleString('en-US')}</div>
-                  <div class="text-sm">{USDollar.format(asset.totalRepay.totalUSD)}</div>
+                </div>
+                {:else}
+                <div class="text-lg"> - </div>
+                {/if}
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-lg text-gray-500">
+                {#if asset.totalRepay.totalToken > 0}
+                <div class="text-lg">{asset.totalRepay.totalToken.toLocaleString('en-US')}</div>
+                <div class="text-sm flex">
+                  <span>{USDollar.format(asset.totalRepay.totalUSD)}</span>
+                  {#if asset.repayChange24h == undefined || asset.repayChange24h > 0}
+                    <p class="flex items-baseline text-sm font-semibold text-green-600">
+                      <!-- Heroicon name: solid/arrow-sm-up -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Increased by
+                      </span>
+                      {`${(asset.repayChange24h).toFixed(2)}%`}
+                    </p>
                   {:else}
-                  <div class="text-lg"> - </div>
+                    <p class="flex items-baseline text-sm font-semibold text-red-600">
+                      <!-- Heroicon name: solid/arrow-sm-down -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Decreased by
+                      </span>
+                      {`${(asset.repayChange24h).toFixed(2)}%`}
+                    </p>
                   {/if}
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                  {#if asset.totalSupply.totalToken > 0}
-                  <div class="text-lg">{asset.totalSupply.totalToken.toLocaleString('en-US')}</div>
-                  <div class="text-sm">{USDollar.format(asset.totalSupply.totalUSD)}</div>
+                </div>
+                {:else}
+                <div class="text-lg"> - </div>
+                {/if}
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {#if asset.totalSupply.totalToken > 0}
+                <div class="text-lg">{asset.totalSupply.totalToken.toLocaleString('en-US')}</div>
+                <div class="text-sm flex">
+                  <span>{USDollar.format(asset.totalSupply.totalUSD)}</span>
+                  {#if asset.supplyChange24h == undefined || asset.supplyChange24h > 0}
+                    <p class="flex items-baseline text-sm font-semibold text-green-600">
+                      <!-- Heroicon name: solid/arrow-sm-up -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Increased by
+                      </span>
+                      {`${(asset.supplyChange24h).toFixed(2)}%`}
+                    </p>
                   {:else}
-                  <div class="text-lg"> - </div>
+                    <p class="flex items-baseline text-sm font-semibold text-red-600">
+                      <!-- Heroicon name: solid/arrow-sm-down -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Decreased by
+                      </span>
+                      {`${(asset.supplyChange24h).toFixed(2)}%`}
+                    </p>
                   {/if}
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap text-lg text-gray-500">
-                  {#if asset.totalWithdraw.totalToken > 0}
-                  <div class="text-lg">{asset.totalWithdraw.totalToken.toLocaleString('en-US')}</div>
-                  <div class="text-sm">{USDollar.format(asset.totalWithdraw.totalUSD)}</div>
+                </div>
+                {:else}
+                <div class="text-lg"> - </div>
+                {/if}
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-lg text-gray-500">
+                {#if asset.totalWithdraw.totalToken > 0}
+                <div class="text-lg">{asset.totalWithdraw.totalToken.toLocaleString('en-US')}</div>
+                <div class="text-sm flex">
+                  <span>{USDollar.format(asset.totalWithdraw.totalUSD)}</span>
+                  {#if asset.withdrawChange24h == undefined || asset.withdrawChange24h > 0}
+                    <p class="flex items-baseline text-sm font-semibold text-green-600">
+                      <!-- Heroicon name: solid/arrow-sm-up -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Increased by
+                      </span>
+                      {`${(asset.withdrawChange24h).toFixed(2)}%`}
+                    </p>
                   {:else}
-                  <div class="text-lg"> - </div>
+                    <p class="flex items-baseline text-sm font-semibold text-red-600">
+                      <!-- Heroicon name: solid/arrow-sm-down -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Decreased by
+                      </span>
+                      {`${(asset.withdrawChange24h).toFixed(2)}%`}
+                    </p>
                   {/if}
-                </td>
-              </tr>
-              {/each}
-              {/if}
-            </tbody>
-        </table>
+                </div>
+                {:else}
+                <div class="text-lg"> - </div>
+                {/if}
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-lg text-gray-500">
+                {#if asset.totalLiquidatedCollateral.token_supply > 0}
+                <div class="text-lg">{asset.totalLiquidatedCollateral.totalToken.toLocaleString('en-US')}</div>
+                <div class="text-sm flex">
+                  <span>{USDollar.format(asset.totalLiquidatedCollateral.totalUSD)}</span>
+                  {#if asset.liquidationChange24h == undefined || asset.liquidationChange24h > 0}
+                    <p class="flex items-baseline text-sm font-semibold text-green-600">
+                      <!-- Heroicon name: solid/arrow-sm-up -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Increased by
+                      </span>
+                      {`${(asset.liquidationChange24h).toFixed(2)}%`}
+                    </p>
+                  {:else}
+                    <p class="flex items-baseline text-sm font-semibold text-red-600">
+                      <!-- Heroicon name: solid/arrow-sm-down -->
+                      <svg class="self-center flex-shrink-0 h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M14.707 10.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 12.586V5a1 1 0 012 0v7.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                      </svg>
+                      <span class="sr-only">
+                        Decreased by
+                      </span>
+                      {`${(asset.liquidationChange24h).toFixed(2)}%`}
+                    </p>
+                  {/if}
+                </div>
+                {:else}
+                <div class="text-lg"> - </div>
+                {/if}
+              </td>
+            </tr>
+            {/each}
+            {/if}
+          </tbody>
+      </table>
           </div>
       </div>
     </div>
